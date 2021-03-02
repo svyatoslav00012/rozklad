fb1eb57f78fdbac3959e888a55d64018d17bf955
svyatoslavshilkov <svyatoslav.shilkov.2012@gmail.com>
2021-03-02 17:06:33 +0200

    fix empty response

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.get('/visits', (req, res) => res.status(200).send(getVisits()+''))
 app.get('/', async (req, res) => {
     incVisits();
     const rozk = await getRozklad()
-    res.status(200).send(rozk.toString())
+    res.status(200).send(rozk.toString() || 'Не вдалося завантажити розклад')
 })
 
 
