a74e43a3fb7ec3b678db6b9b5745c8a32bdee414
svyatoslavshilkov <svyatoslav.shilkov.2012@gmail.com>
2021-03-02 17:18:06 +0200

    fix empty response

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,6 @@
 const Config = {
-    VISITS_FILE: './files/visits',
-    ROZKLAD_FILE: './files/rozklad.html',
+    VISITS_FILE: '/files/visits',
+    ROZKLAD_FILE: '/files/rozklad.html',
     FETCH_URL: "https://student.lpnu.ua/students_schedule?departmentparent_abbrname_selective=%D0%86%D0%9A%D0%9D%D0%86&studygroup_abbrname_selective=%D0%9A%D0%9D-409&semestrduration=1"
 }
 
