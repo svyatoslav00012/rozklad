901e580d1505123d695f06c593ee1065b1276bfc
svyatoslavshilkov <svyatoslav.shilkov.2012@gmail.com>
2021-03-06 23:16:10 +0200

    backup cache added

--- src/Config.js
+++ src/Config.js
@@ -6,7 +6,8 @@ const Config = {
         port: 6379,
         host: process.env.REDIS_HOST || 'localhost'
     },
-    ROZKLAD_UPDATE_INTERVAL: 6 * 60 * 60 * 1000,
+    ROZKLAD_EXPIRE: 3600,
+    ROZKLAD_UPDATE_INTERVAL: 6 * 3600 * 1000,
     FETCH_URL: (institute, group) => `https://student.lpnu.ua/students_schedule?departmentparent_abbrname_selective=${institute}&studygroup_abbrname_selective=${group}&semestrduration=1`
 }
 
--- src/services/rozkladService.js
+++ src/services/rozkladService.js
@@ -27,28 +27,44 @@ function rozkladService({redis, config, axios, htmlParsingService}) {
         return cached
     }
 
+    const cacheRozklad = async (group, rozklad, json) => {
+        await redis.setAsync(`rozklad:${group}`, rozklad, 'EX', config.ROZKLAD_EXPIRE)
+        await redis.setAsync(`rozklad-json:${group}`, JSON.stringify(json), 'EX', config.ROZKLAD_EXPIRE)
+        await redis.setAsync(`backup:rozklad:${group}`, rozklad)
+        await redis.setAsync(`backup:rozklad-json:${group}`, JSON.stringify(json))
+    }
+
     const fetchAndCacheRozklad = async group => {
         const inst = await getInstitute(group)
         const fetched = await fetchRozklad(inst, group)
         if (fetched) {
             const json = htmlParsingService.extractRozkladData(fetched)
-            await redis.setAsync(`rozklad:${group}`, fetched, 'EX', 3600)
-            await redis.setAsync(`rozklad-json:${group}`, JSON.stringify(json), 'EX', 3600)
+            await cacheRozklad(group, fetched, json)
             return {json, html: fetched}
         } else return null
     }
 
     const getRozklad = async (group) => {
         const cached = await redis.getAsync(`rozklad:${group}`)
-        if (!cached)
-            return fetchAndCacheRozklad(group)?.html
+        if (!cached) {
+            const fetched = await fetchAndCacheRozklad(group)?.html
+            if(!fetched){
+                return await redis.getAsync(`backup:rozklad:${group}`)
+            }
+            return fetched
+        }
         return cached
     }
 
     const getRozkladData = async (group) => {
         const cached = JSON.parse(await redis.getAsync(`rozklad-json:${group}`))
-        if (!cached)
-            return fetchAndCacheRozklad(group)?.json
+        if (!cached) {
+            const fetched = await fetchAndCacheRozklad(group)?.json
+            if(!fetched){
+                return await redis.getAsync(`backup:rozklad-json:${group}`)
+            }
+            return fetched
+        }
         return cached
     }
 
