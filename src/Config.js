eb1d089326a4962b7cda07cc0cef3c146e4e9bd0
svyatoslavshilkov <svyatoslav.shilkov.2012@gmail.com>
2021-03-06 17:16:44 +0200

    fix redis host

--- src/Config.js
+++ src/Config.js
@@ -4,7 +4,7 @@ const appDir = path.dirname(require.main.filename);
 const Config = {
     redis: {
         port: 6379,
-        host: 'localhost'
+        host: process.env.REDIS_HOST || 'localhost'
     },
     ROZKLAD_UPDATE_INTERVAL: 6 * 60 * 60 * 1000,
     FETCH_URL: (institute, group) => `https://student.lpnu.ua/students_schedule?departmentparent_abbrname_selective=${institute}&studygroup_abbrname_selective=${group}&semestrduration=1`
